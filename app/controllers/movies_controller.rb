d25f029211c3405a6ffe25c882dc4d6eeadc5634
hurik zamith <hurikzamith@gmail.com>
2023-04-13 10:29:38 -0300

    remove user reference

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,22 @@
-require 'csv'
-
 class MoviesController < ApplicationController
-  def create_from_csv
-    csv_text = File.read(Rails.root.join('public', 'netflix_titles.csv'))
-    csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
 
+  def read_csv
+
+    require 'csv'
+    csv_text = File.read('netflix_titles.csv')
+    csv = CSV.parse(csv_text, headers: true)
     csv.each do |row|
-      Movie.create!(
-        title: row['title'],
-        description: row['description'],
-        country: row['country'],
-        year: row['release_year'],
-        genre: row['listed_in']
-      )
+      Movie.find_or_create_by(title: row['title'], genre: row['listed_in'], year: row['release_year'], country: row['country'], published_at: row['date_added'], description: row['description'])
     end
+    render json: { message: "Dados do CSV importados com sucesso!" }
+  end
 
-    render json: { message: "Filmes criados a partir do arquivo CSV com sucesso!" }
+  def index
+    movies = Movie.order(year: :asc)
+    movies = movies.where(year: params[:year]) if params[:year].present?
+    movies = movies.where(genre: params[:genre]) if params[:genre].present?
+    movies = movies.where(country: params[:country]) if params[:country].present?
+    render json: movies
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'movies/read_csv'
+  get 'movies', to: 'movies#index'
+  # resources :movies, only: [:index]
 
-  
+  # post 'movies/create_from_csv'
+  # resources :movies, only:
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
